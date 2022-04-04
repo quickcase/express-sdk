0992e68a40031576f22d664216580d5f8ff31483
Valentin Laurin <valentin.laurin@quickcase.app>
2022-04-04 17:35:50 +0100

    Add session-based openid request authentication
    Implementation of request authentication function for OpenId guard based on presence of a token set in req.session.openId

---
+++ src/openid/authenticate.js
@@ -0,0 +1,99 @@
+import {TokenSet} from 'openid-client';
+import {Logger} from '../logging';
+
+const ACCESS_TOKEN_HINT = 'access_token';
+const logger = Logger('openid:authenticate');
+
+/**
+ * Introspect a token set to assess validity against expiry.
+ * @param {object} deps - Dependencies
+ * @returns {boolean} True if tokenSet is active, false otherwise
+ */
+export const defaultTokenSetIntrospector = (deps) => async (tokenSet) => {
+  const {client} = deps;
+  if (tokenSet.expires_at) {
+    return !tokenSet.expired();
+  } else {
+    try {
+      const introspection = await client.introspect(tokenSet.access_token, ACCESS_TOKEN_HINT);
+      return introspection.active;
+    } catch (error) {
+      logger.warn('Error while introspecting access token because of:', error);
+    }
+  }
+
+  return false;
+};
+
+export const ERROR = Object.freeze({
+  NO_SESSION: 'no_session',
+  NO_TOKEN: 'no_token',
+  EXPIRED_TOKEN: 'expired_token',
+  FAILED_REFRESH: 'failed_refresh',
+});
+
+/**
+ * Authenticate a request based on the presence of a valid TokenSet in `req.session.openId`.
+ */
+export const authenticateFromSession = (deps) => async (req, res) => {
+  const {
+    tokenSetIntrospector,
+    tokenSetRefresher,
+  } = deps;
+
+  const sessionTokenSet = req.session?.openId?.tokenSet;
+
+  if (!sessionTokenSet || typeof sessionTokenSet !== 'object') {
+    return {
+      authenticated: false,
+      error: ERROR.NO_SESSION,
+      errorDescription: 'No session found',
+    };
+  }
+
+  if (!sessionTokenSet.access_token) {
+    return {
+      authenticated: false,
+      error: ERROR.NO_TOKEN,
+      errorDescription: 'No access token in session',
+    };
+  }
+
+  const tokenSet = new TokenSet(sessionTokenSet);
+
+  const active = await tokenSetIntrospector(tokenSet);
+
+  if (!active) {
+    if (!tokenSet.refresh_token) {
+      return {
+        authenticated: false,
+        error: ERROR.EXPIRED_TOKEN,
+        errorDescription: 'Access token is expired',
+      };
+    }
+
+    try {
+      const refreshedTokenSet = await tokenSetRefresher(tokenSet);
+      req.session.openId = {
+        ...req.session.openId,
+        tokenSet: {
+          // Preserve id_token in case it's omitted from refresh response
+          id_token: tokenSet.id_token,
+          // Apply refreshed tokenSet
+          ...refreshedTokenSet,
+        },
+      };
+    } catch (error) {
+      logger.warn('Error while attempting refresh request because of:', error);
+      return {
+        authenticated: false,
+        error: ERROR.FAILED_REFRESH,
+        errorDescription: 'The authorization server rejected the refresh request',
+      };
+    }
+  }
+
+  return {
+    authenticated: true,
+  };
+};
---
+++ src/openid/authenticate.test.js
@@ -0,0 +1,220 @@
+import {givenMiddleware} from '@quickcase/node-toolkit/test';
+import {TokenSet} from 'openid-client';
+import {authenticateFromSession, defaultTokenSetIntrospector} from './authenticate';
+
+describe('defaultTokenSetIntrospector', () => {
+  test('should not be active when `expires_at` in the past', async () => {
+    const tokenSet = new TokenSet({
+      access_token: '123',
+    });
+    const introspect = defaultTokenSetIntrospector({});
+
+    tokenSet.expires_in = -60;
+
+    await expect(introspect(tokenSet)).resolves.toBe(false);
+  });
+
+  test('should be active when `expires_at` in future', async () => {
+    const tokenSet = new TokenSet({
+      access_token: '123',
+    });
+    const introspect = defaultTokenSetIntrospector({});
+
+    tokenSet.expires_in = 60;
+
+    await expect(introspect(tokenSet)).resolves.toBe(true);
+  });
+
+  test('should not be active when no `expires_at` and introspection `active` flag is false', async () => {
+    const tokenSet = new TokenSet({
+      access_token: '123',
+    });
+    const client = {
+      introspect: jest.fn().mockResolvedValue({active: false}),
+    };
+    const introspect = defaultTokenSetIntrospector({client});
+
+    await expect(introspect(tokenSet)).resolves.toBe(false);
+    expect(client.introspect).toHaveBeenCalledWith('123', 'access_token');
+  });
+
+  test('should be active when no `expires_at` and introspection `active` flag is true', async () => {
+    const tokenSet = new TokenSet({
+      access_token: '123',
+    });
+    const client = {
+      introspect: jest.fn().mockResolvedValue({active: true}),
+    };
+    const introspect = defaultTokenSetIntrospector({client});
+
+    await expect(introspect(tokenSet)).resolves.toBe(true);
+    expect(client.introspect).toHaveBeenCalledTimes(1);
+  });
+
+  test('should not be active when introspection fails', async () => {
+    const tokenSet = new TokenSet({
+      access_token: '123',
+    });
+    const client = {
+      introspect: jest.fn().mockRejectedValue(new Error('introspection error')),
+    };
+    const introspect = defaultTokenSetIntrospector({client});
+
+    await expect(introspect(tokenSet)).resolves.toBe(false);
+    expect(client.introspect).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('authenticateFromSession', () => {
+  const fromSession = (session) => ({session});
+  const fromOpenIdSession = (openId) => fromSession({openId});
+
+  [
+    undefined,
+    {},
+    {openId: null},
+    {openId: 'xxx'},
+    {openId: {tokenSet: null}},
+    {openId: {tokenSet: 'xxx'}},
+  ].forEach(session => {
+    test(`should not be authenticated when invalid openId session: ${JSON.stringify(session)}`, async () => {
+      const authenticate = authenticateFromSession({});
+
+      const req = fromSession(session);
+
+      await expect(authenticate(req)).resolves.toEqual({
+        authenticated: false,
+        error: 'no_session',
+        errorDescription: 'No session found',
+      });
+    });
+  });
+
+  test('should not be authenticated when no access token', async () => {
+    const authenticate = authenticateFromSession({});
+
+    const req = fromOpenIdSession({tokenSet: {token_type: 'Bearer'}});
+
+    await expect(authenticate(req)).resolves.toEqual({
+      authenticated: false,
+      error: 'no_token',
+      errorDescription: 'No access token in session',
+    });
+  });
+
+  test('should not be authenticated when access token not active and no refresh token', async () => {
+    const authenticate = authenticateFromSession({
+      tokenSetIntrospector: jest.fn().mockResolvedValue(false),
+    });
+
+    const req = fromOpenIdSession({
+      tokenSet: {
+        access_token: '123',
+      },
+    });
+
+    await expect(authenticate(req)).resolves.toEqual({
+      authenticated: false,
+      error: 'expired_token',
+      errorDescription: 'Access token is expired',
+    });
+  });
+
+  test('should be authenticated when access token is active', async () => {
+    const authenticate = authenticateFromSession({
+      tokenSetIntrospector: jest.fn().mockResolvedValue(true),
+    });
+
+    const req = fromOpenIdSession({
+      tokenSet: {
+        access_token: '123',
+      },
+    });
+
+    await expect(authenticate(req)).resolves.toEqual({
+      authenticated: true,
+    });
+  });
+
+  test('should refresh token set when access token not active', async () => {
+    const authenticate = authenticateFromSession({
+      tokenSetIntrospector: jest.fn().mockResolvedValue(false),
+      tokenSetRefresher: jest.fn().mockResolvedValue({
+        access_token: 'new-access-token',
+        expires_at: 9999999999999,
+        id_token: 'new-id-token',
+        refresh_token: '789',
+      }),
+    });
+
+    const req = fromOpenIdSession({
+      tokenSet: {
+        access_token: '123',
+        id_token: 'id-123',
+        refresh_token: '456',
+        expires_at: 1,
+      },
+    });
+
+    await expect(authenticate(req)).resolves.toEqual({
+      authenticated: true,
+    });
+
+    expect(req.session.openId.tokenSet).toEqual({
+      access_token: 'new-access-token',
+      expires_at: 9999999999999,
+      id_token: 'new-id-token',
+      refresh_token: '789',
+    });
+  });
+
+  test('should preserve id_token when missing from refreshed token set', async () => {
+    const authenticate = authenticateFromSession({
+      tokenSetIntrospector: jest.fn().mockResolvedValue(false),
+      tokenSetRefresher: jest.fn().mockResolvedValue({
+        access_token: 'new-access-token',
+        expires_at: 9999999999999,
+      }),
+    });
+
+    const req = fromOpenIdSession({
+      tokenSet: {
+        access_token: '123',
+        id_token: 'id-123',
+        refresh_token: '456',
+        expires_at: 1,
+      },
+    });
+
+    await expect(authenticate(req)).resolves.toEqual({
+      authenticated: true,
+    });
+
+    expect(req.session.openId.tokenSet).toEqual({
+      access_token: 'new-access-token',
+      expires_at: 9999999999999,
+      id_token: 'id-123',
+    });
+  });
+
+  test('should not be authenticated when refresh fails', async () => {
+    const authenticate = authenticateFromSession({
+      tokenSetIntrospector: jest.fn().mockResolvedValue(false),
+      tokenSetRefresher: jest.fn().mockRejectedValue(new Error('Failed to refresh')),
+    });
+
+    const req = fromOpenIdSession({
+      tokenSet: {
+        access_token: '123',
+        refresh_token: '456',
+        expires_at: 1,
+      },
+    });
+
+    await expect(authenticate(req)).resolves.toEqual({
+      authenticated: false,
+      error: 'failed_refresh',
+      errorDescription: 'The authorization server rejected the refresh request',
+    });
+  });
+});
