13ab2081c38ea54f619d91e1f7b3dcc214d3346f
Valentin Laurin <valentin.laurin@quickcase.app>
2022-04-05 09:59:05 +0100

    Add Logout middleware
    Handles destruction of session and OpenID RP-initiated logout

---
+++ src/openid/logout.js
@@ -0,0 +1,25 @@
+import {Logger} from '../logging';
+
+const logger = Logger('openid:logout');
+
+/**
+ * Express middleware handling destruction of session and OpenID RP-initiated logout.
+ */
+export const logoutMiddleware = (deps) => (config) => async (req, res) => {
+  const {
+    endSessionUrlSupplier,
+  } = deps;
+  // const {} = config;
+
+  const endSessionUrl = endSessionUrlSupplier({
+    id_token_hint: req.session?.openId?.tokenSet?.id_token,
+  });
+
+  req.session.destroy((err) => {
+    if (err) {
+      logger.warn(`Error while destroying session ${req.session.id}:`, err);
+    }
+
+    return res.redirect(endSessionUrl);
+  });
+};
---
+++ src/openid/logout.test.js
@@ -0,0 +1,63 @@
+import {givenMiddleware} from '@quickcase/node-toolkit/test';
+import {logoutMiddleware} from './logout';
+
+const config = {};
+
+const deps = {
+  endSessionUrlSupplier: ({id_token_hint}) => `http://op/session/end?id_token_hint=${id_token_hint}`,
+};
+
+test('should destroy session', async () => {
+  const middleware = logoutMiddleware(deps)(config);
+
+  const req = {
+    session: {
+      destroy: jest.fn((cb) => cb()),
+    }
+  };
+
+  await givenMiddleware(middleware).when(req).expectResponse();
+
+  expect(req.session.destroy).toHaveBeenCalledTimes(1);
+});
+
+test('should redirect to end session URL', async () => {
+  const middleware = logoutMiddleware(deps)(config);
+
+  const req = {
+    session: {
+      destroy: jest.fn((cb) => cb()),
+      openId: {
+        tokenSet: {
+          id_token: 'id-token-123',
+        },
+      },
+    }
+  };
+
+  const res = await givenMiddleware(middleware).when(req).expectResponse();
+
+  expect(res.status).toEqual(302);
+  expect(res.redirect).toEqual('http://op/session/end?id_token_hint=id-token-123');
+});
+
+test('should silently handle session destruction error', async () => {
+  const middleware = logoutMiddleware(deps)(config);
+
+  const req = {
+    session: {
+      id: '123',
+      destroy: jest.fn((cb) => cb(new Error('Session could not be destroyed'))),
+      openId: {
+        tokenSet: {
+          id_token: 'id-token-123',
+        },
+      },
+    }
+  };
+
+  const res = await givenMiddleware(middleware).when(req).expectResponse();
+
+  expect(res.status).toEqual(302);
+  expect(res.redirect).toEqual('http://op/session/end?id_token_hint=id-token-123');
+});
