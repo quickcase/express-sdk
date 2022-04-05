011823be64e03f9d7a6ea726726acefa96b5b03b
Valentin Laurin <valentin.laurin@quickcase.app>
2022-04-05 13:19:48 +0100

    Add default OpenID prompt supplier
    Given an authentication error, select an OpenID prompt to use for a new authentication flow.

---
+++ src/openid/prompt-supplier.test.js
@@ -0,0 +1,42 @@
+import {ERROR} from './authenticate';
+import {defaultPromptSupplier} from './prompt-supplier';
+
+describe('defaultPromptSupplier', () => {
+  test('should return undefined when no prompt set', async () => {
+    const config = undefined;
+    const prompt = defaultPromptSupplier(config)({});
+
+    expect(prompt).toBe(undefined);
+  });
+
+  test('should return default prompt', async () => {
+    const config = {
+      default: 'login consent',
+      expired: 'none',
+    };
+    const prompt = defaultPromptSupplier(config)({});
+
+    expect(prompt).toBe('login consent');
+  });
+
+  describe('when auth error is `expired_token`', () => {
+    test('should return undefined when not set', async () => {
+      const config = {
+        default: 'login consent',
+      };
+      const prompt = defaultPromptSupplier(config)({error: ERROR.EXPIRED_TOKEN});
+
+      expect(prompt).toBe(undefined);
+    });
+
+    test('should return expired prompt when set', async () => {
+      const config = {
+        default: 'login consent',
+        expired: 'none',
+      };
+      const prompt = defaultPromptSupplier(config)({error: ERROR.EXPIRED_TOKEN});
+
+      expect(prompt).toBe('none');
+    });
+  });
+});
